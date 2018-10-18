2b397c394a71886678e5a029b6cf557827c1d527
Shubhankar <dimrishubhi@gmail.com>
2018-10-18 09:49:24 +0530

    some screen width specific modifications

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -11,7 +11,13 @@ export class CoinsComponent implements OnInit {
 
   constructor( private stocksService: StocksService ) { }
 
+  // Getting screen width
+  screen_width = screen.width;
+
+  // Get coins data
   coins = this.stocksService.getCoins().subscribe( d => { this.coins = d; } )[0];
+
+  // Columns to display
   displayedColumns: String[] = ['sno', 'name', 'curr_price', 'per_chn_1h', 'per_chn_24h', 'per_chn_7d', 'mrk_cap'];
 
   ngOnInit() {
