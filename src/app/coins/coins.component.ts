c9295d4c91c10733420428da0d70201bdd10d95d
Shubhankar <dimrishubhi@gmail.com>
2018-10-22 23:00:09 +0530

    heroku deployment updates

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -11,14 +11,7 @@ export class CoinsComponent implements OnInit {
 
   constructor( private stocksService: StocksService ) { }
 
-  // Getting screen width
-  screen_width = screen.width;
-
-  // Get coins data
-  coins = this.stocksService.getCoins().subscribe( d => { this.coins = d; } )[0];
-
-  // Columns to display
-  displayedColumns: String[] = ['sno', 'name', 'curr_price', 'per_chn_1h', 'per_chn_24h', 'per_chn_7d', 'mrk_cap'];
+  coins = this.stocksService.getCoins().subscribe( d => this.coins = d )[0];
 
   ngOnInit() {
   }
